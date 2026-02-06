e4a97ff2d7020e9ae84b1b16fc0e20efbe26127b
workingdog <ringowathelet@gmail.com>
2026-02-06 11:11:48 +0900

    Initial Commit

---
+++ Directus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Directus
+//
+//  Created by Ringo Wathelet on 2026/02/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Directus/DirectusApp.swift
@@ -0,0 +1,17 @@
+//
+//  DirectusApp.swift
+//  Directus
+//
+//  Created by Ringo Wathelet on 2026/02/06.
+//
+
+import SwiftUI
+
+@main
+struct DirectusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
